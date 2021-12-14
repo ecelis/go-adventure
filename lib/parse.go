1c4c50bf9f618f9892446b7714bda53971dd435a
Ernesto Celis <ecelisdelafuente@pentalog.com>
2021-12-13 18:00:56 -0600

    Add executeLook and executeGo functions WIP

--- lib/parse.go
+++ lib/parse.go
@@ -5,21 +5,64 @@ import (
 	"strings"
 )
 
+type location struct {
+	description string
+	tag         string
+}
+
+/*
+	locs := []location{
+		location{description: "an open field", tag: "field"},
+		location{description: "a little cave", tag: "cave"},
+	}
+
+*/
+var locs = make([]location, 2)
+var playerLocation int = 0
+
 func Execute(input string) bool {
-	var verb []string = strings.Split(input, " ")
+	var token []string = strings.Split(input, " ")
 	// var noun = "\n"
-	fmt.Println(verb)
-	if len(verb) > 0 {
-		switch verb[0] {
+	fmt.Println(token)
+	if len(token) > 0 {
+		switch token[0] {
 		case "quit":
 			return false
 		case "look":
-			fmt.Println("It is very dark in here.")
+			// fmt.Println("It is very dark in here.")
+			executeLook(token[1])
 		case "go":
-			fmt.Println("It's too dark to go anywhere.")
+			// fmt.Println("It's too dark to go anywhere.")
+			executeGo(token[1])
 		default:
-			fmt.Printf("I don't know how to %s", verb[0])
+			fmt.Printf("I don't know how to %s", token[0])
 		}
 	}
 	return true
+}
+
+func executeLook(noun string) {
+	switch noun {
+	case "around":
+		fmt.Printf("Your are in %s.\n", locs[playerLocation].description)
+	default:
+		fmt.Println("I don't undestand what you want to see.")
+	}
+}
+
+func executeGo(noun string) {
+	for i := 0; i < len(locs); i++ {
+		if noun != "" && noun == locs[i].tag {
+			switch i {
+			case playerLocation:
+				fmt.Println("You can't get much closer than this.")
+			default:
+				fmt.Println("OK")
+				playerLocation = i
+				executeLook("around")
+			}
+			return
+		}
+	}
+	fmt.Println("I don't understand where you want to go.")
 }
